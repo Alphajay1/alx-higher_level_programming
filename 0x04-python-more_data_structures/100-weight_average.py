c16f35853ee0f2dd2a43a2e1ccf169e7cec93a65
Alphajay1 <ayakojamesty@gmail.com>
2024-01-10 19:08:02 -0800

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ def weight_average(my_list=[]):
     if len(my_list) == 0:
         return (0)
 
-    return (sum([mul(i[0], i[1]) for i in my_list]) / sum(i[1] for i in my_list))
+    return ((sum([mul(i[0], i[1]) for i in my_list])) / (sum(i[1] for i in my_list)))
 
 def mul(i, j):
     return (i * j)
