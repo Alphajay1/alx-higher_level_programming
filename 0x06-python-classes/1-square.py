47c11dfbb1e48a79a96e78d646c299a2798e6b8e
Alphajay1 <ayakojamesty@gmail.com>
2024-01-23 07:40:22 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class Square based on 0-square.py by
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+"""
+
+class Square:
+    """Defines a square
+
+    Attributes:
+        __size(int): size of one side of the square
+    """
+
+    def __init__(self, size):
+        """Initializes a square
+
+        Args:
+            size(int): size of one side of a square
+
+        Returns: None
+        """
+
+        self.__size = size
