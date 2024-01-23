64ae6ec87bf209a6f47ff00ff1f3749e0bb9c308
Alphajay1 <ayakojamesty@gmail.com>
2024-01-23 10:36:07 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/pythom3
+"""Defines a square based on 2-square.py"""
+
+
+class Square:
+    """Represents a square
+
+    Attributes:
+        __size(int): size of one side of a square
+    """
+    def __init__(self, size=0):
+        """Initialize the square
+
+        Args:
+            size(int): size of one side of the square
+
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+
+            else:
+                self.__size = size
+
+def area(self):
+    """Calculates the area of the square
+
+    Returns:
+        the area of the square
+    """
+    return ((self.__size) ** 2)
