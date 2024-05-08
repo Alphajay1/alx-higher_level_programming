7a16990dc190185f53597766dbca0c4609ea4f1d
Alphajay1 <ayakojamesty@gmail.com>
2024-05-08 04:33:01 -0700

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,8 +4,8 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-     let row = '';
-     for (let j = 0; j < size; j++) row += 'X';
+    let row = '';
+    for (let j = 0; j < size; j++) row += 'X';
      console.log(row);
   }
 }
